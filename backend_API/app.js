b6df15b82196d67c307971dbdd9627623be25960
Marcel Cornett <marcornett@gmail.com>
2020-07-14 17:51:01 -0400

    Added heroku

--- backend_API/app.js
+++ backend_API/app.js
@@ -1,18 +1,23 @@
 import express from 'express'
 import mongoose from 'mongoose'
+import path from 'path'
 import { Schema } from 'mongoose'
 
 // Start App
+const port = process.env.PORT || 4000
+const current_server_directory = path.resolve(path.dirname("."))
+const static_directory = path.resolve(current_server_directory, "backend_API", "public")
+
+
 const app = express()
-const port = 3000
+// Middleware
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(express.static(static_directory))
 
 app.get('/', (req, res) => {
     res.send('Capstone Backend')
 })
-
-// Middleware
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
 
 // App Listening
 app.listen(port, () => {
